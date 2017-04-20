47dab46465826747047b8be098270ab62defdb54
Housam Khalifa Bashier <me.the.fren@gmail.com>
2017-04-20 15:38:12 -0600

    Update extract_principle_components.m

--- Ink Classification/extract_principle_components.m
+++ Ink Classification/extract_principle_components.m
@@ -1,24 +1,18 @@
-function [pca_train pca_test] = extract_principle_components(training_set, testing_set,p_components)
-train_size = length(training_set(1,:));
-test_size = length(testing_set(1,:));
-%% mean image and mean-shifted image
-mean_digits = mean(training_set,2);
-shifted_images = training_set-repmat(mean_digits, 1,train_size);
-%% ordered eigenvectors and eigenvalues
+function [pca_train pca_test] = extract_principle_components(training_set, testing_set,num_com)
+kk = training_set-repmat(mean(training_set,2), 1,length(training_set(1,:)));
 [eigen_vectors, score, eigen_values] = princomp(training_set');
-%% return the top eigenvectors i.e. principle components
-eigen_vectors = eigen_vectors(:,1:p_components);
-% save train_eigen_vect_25 eigen_vectors;
-% save train_shifted_img_25 shifted_images;
-% save train_mean_25 mean_digits;
-%% project images into subspace to generate feature vectors
-train_features = eigen_vectors'*shifted_images;
+eigen_vectors = eigen_vectors(:,1:num_com);
+
+ save train_eigen_vect_25 eigen_vectors;
+ save train_shifted_img_25 shifted_images;
+ save train_mean_25 mean_digits;
+ 
+train_features = eigen_vectors'*kk;
 pca_train = train_features;
-%% test features
 pca_test = [];
-for y=1:test_size
+for y=1:length(testing_set(1,:))
    cur_img = testing_set(:,y);
-   test_feature = eigen_vectors' *(cur_img-mean_digits);
+   test_feature = eigen_vectors' *(cur_img-mean(training_set,2));
    pca_test(:,y) = test_feature;
 end
 pca_train = pca_train';
@@ -27,5 +21,5 @@ normplot(pca_train(:))
 [U,S,V] = svd(training_set);
 diag_res  = diag(S);
 var_res = sum(diag_res(1:p_components))/sum(diag_res);
-fprintf('Variance : ', var_res);
+fprintf('Variance : %d', var_res);
 end
